5b7b1737c127dd01ca9d2c97469036e44a7de66e
rowan kavanagh <rowkav0809@highgateschool.org.uk>
2026-04-30 14:57:25 +0000

    feat: started handling inputs for searching the json

--- test_skins_json_api.py
+++ test_skins_json_api.py
@@ -1,5 +1,6 @@
 import requests
 import tabulate
+from item_definition_indexes import weapons
 headers = {
 }
 params = {
@@ -20,25 +21,53 @@ except requests.RequestException as e:
     print(f'Failed to fetch listings: {e}')
 
 
-
-filtered = [
-    skin for skin in data
-    if skin.get('weapon', {}).get('weapon_id') == 507
-]
-tabulated = [['item','rarity','case']]
-
-for skin in filtered:
-    name = f'{(skin.get('weapon') or {}).get('name')} | {(skin.get('pattern') or {}).get('name')}'
-    rarity = f'{(skin.get('rarity') or {}).get('name')}'
-    crate = skin.get('crates') or []
-    crate_name = crate[0].get('name') if crate else None
-    info = [name,rarity,crate_name]
-    tabulated.append(info)
-final = []
-for item in tabulated:
-    if item not in final:
-        final.append(item)
+def get_id(name):
+    for item in weapons:
+        if item[1].lower() == name.lower():
+            return item[0]
+    return('ivalid item name')
+        
 
 
-print(tabulate.tabulate(final))
+
+
+def get_info(name,finish,rarity,crate_n):
+    filtered = [
+        skin for skin in data
+        if skin.get('weapon', {}).get('weapon_id') == 507
+    ]
+    tabulated = [['item','rarity','case']]
+
+    for skin in filtered:
+        
+        if finish and name:
+            name = f'{(skin.get('weapon') or {}).get('name')==name} | {(skin.get('pattern') or {}).get('name') == finish}'
+        elif finish:
+            name = f'{(skin.get('weapon') or {}).get('name')} | {(skin.get('pattern') or {}).get('name')==finish}'
+        elif name:
+            name = f'{(skin.get('weapon') or {}).get('name')==name} | {(skin.get('pattern') or {}).get('name')}'
+        else:
+            name = f'{(skin.get('weapon') or {}).get('name')} | {(skin.get('pattern') or {}).get('name')}'
+        
+        if rarity:
+            rarity = f'{(skin.get('rarity') or {}).get('name')==rarity}'
+        else:
+            rarity = f'{(skin.get('rarity') or {}).get('name')==rarity}'
+            
+        crate = skin.get('crates') or []
+        if crate:
+            for item in crate:
+                crate_name = crate.get('name')==crate_n if crate else None
+        else:
+            crate_name = crate[0].get('name') if crate else None
+        info = [name,rarity,crate_name]
+        tabulated.append(info)
+
+    final = []
+    for item in tabulated:
+        if item not in final:
+            final.append(item)
+
+
+    print(tabulate.tabulate(final))
 

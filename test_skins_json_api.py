519b429d58867a5d2e72f17fab31810e180e879b
rowan kavanagh <rowkav0809@highgateschool.org.uk>
2026-05-13 09:31:56 +0000

    Refactor get_info function and enhance CLI for improved skin retrieval options

--- test_skins_json_api.py
+++ test_skins_json_api.py
@@ -6,49 +6,40 @@ import urllib.request
 from PIL import Image
 import json
 
-headers = {
-}
-params = {
-    'weapon_id': '507'
-    
-}
-
-
-
-
 with open('skins.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 def get_id(name):
     for item in weapons:
         if item[1].lower() == name.lower():
             return item[0]
-    return('ivalid item name')
-        
-
-
-
+    return('invalid item name')
 
 def get_info(name=None, finish=None, rarity=None, crate_n=None, weapon_id=None):
 
     # convert weapon name to id
-    if weapon_id is None:
+    if weapon_id is None and name is not None:
         if name:
             weapon_id = get_id(name)
         else:
             print('Please provide a weapon name or weapon id')
             return
 
+
     # invalid name check
     if isinstance(weapon_id, str):
         print('Invalid weapon name')
         return
+    if weapon_id is not None:
+        filtered = [
+            skin for skin in data
+            if skin.get('weapon', {}).get('weapon_id') == weapon_id
+        ]
+    else:
+        filtered = [
+            skin for skin in data
+        ]
 
-    filtered = [
-        skin for skin in data
-        if skin.get('weapon', {}).get('weapon_id') == weapon_id
-    ]
-
-    table = [['item', 'rarity', 'case']]
+    table = [['Item','|','Finish', 'Rarity', 'Case']]
 
     for skin in filtered:
 
@@ -57,8 +48,7 @@ def get_info(name=None, finish=None, rarity=None, crate_n=None, weapon_id=None):
         rarity_name = (skin.get('rarity') or {}).get('name')
         pattern_name = pattern_name if pattern_name is not None else "Vanilla"
         crates = skin.get('crates') or []
-
-        # optional filters
+        
         if pattern_name is not None:
             if finish and pattern_name.lower() != finish.lower():
                 continue
@@ -80,14 +70,53 @@ def get_info(name=None, finish=None, rarity=None, crate_n=None, weapon_id=None):
 
                 crate_name = current_crate
                 break
+        if crate_name is None:
+            crate_name = "Collection"
+        #item_name = f'{weapon_name} | {pattern_name}'
 
-        item_name = f'{weapon_name} | {pattern_name}'
-
-        table.append([item_name, rarity_name, crate_name])
+        table.append([weapon_name,'|',pattern_name, rarity_name, crate_name])
 
     print(tabulate.tabulate(table))
 
-get_info(name='r8 revolver')
+def cli():
+    valid = True 
+    while valid:
+        print('1. Get skins by weapon name')
+        print('2. Get skins by weapon id')
+        print('3. get skins by weapon name and finish')
+        print('4. get skins by weapon name and rarity')
+        print('5. get skins by weapon name and crate')
+        print('6. get skins by weapon finish')
+        print('7. get skins by weapon rarity')
+        choice = input('Enter your choice: ')
+        if choice == '1':
+            name = input('Enter weapon name: ')
+            get_info(name=name)
+        elif choice == '2':
+            weapon_id = int(input('Enter weapon id: '))
+            get_info(weapon_id=weapon_id)
+        elif choice == '3':
+            name = input('Enter weapon name: ')
+            finish = input('Enter finish: ')
+            get_info(name=name, finish=finish)
+        elif choice == '4':
+            name = input('Enter weapon name: ')
+            rarity = input('Enter rarity: ')
+            get_info(name=name, rarity=rarity)
+        elif choice == '5':
+            name = input('Enter weapon name: ')
+            crate_n = input('Enter crate name: ')
+            get_info(name=name, crate_n=crate_n)
+        elif choice == '6':
+            finish = input('Enter finish: ')
+            get_info(finish=finish)
+        elif choice == '7':
+            rarity = input('Enter rarity: ')
+            get_info(rarity=rarity)
+        else:
+            print('Invalid choice')
+
+cli()
 
 # url = 'https://community.akamai.steamstatic.com/economy/image/i0CoZ81Ui0m-9KwlBY1L_18myuGuq1wfhWSaZgMttyVfPaERSR0Wqmu7LAocGJKz2lu_XsnXwtmkJjSU91dh8bj35VTqVBP4io_fr3EVvKD6MKU_cKPKXWHFxLkls7FsSnDqwUl_sWTczoqheHifbwMmD5F1RvlK7Ec_KL6Q_A'
 # urllib.request.urlretrieve(
